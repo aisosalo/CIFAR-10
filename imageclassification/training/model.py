b22997bf19d4673c9bdfb4d4d855806bfdab6efa
Antti Isosalo <34138494+aisosalo@users.noreply.github.com>
2019-01-22 21:05:43 +0200

    Update model.py

--- imageclassification/training/model.py
+++ imageclassification/training/model.py
@@ -1,1 +1,11 @@
+import imageclassification.training.models as mdls
 
+
+def get_model(my_net, num_classes):
+    if 'vggbndrop' in my_net:
+        net = mdls.VGGBNDrop(num_classes=num_classes, init_weights=True)
+    elif 'vgg' in my_net:
+        net = mdls.VGG(num_classes=num_classes, init_weights=True)
+    else:
+        raise NotImplementedError
+    return net
