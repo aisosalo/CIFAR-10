c3a248bea135c1cb6069ddafe78d14ca1506c16e
Antti Isosalo <34138494+aisosalo@users.noreply.github.com>
2019-01-23 17:05:53 +0200

    Update dataset.py

--- imageclassification/training/dataset.py
+++ imageclassification/training/dataset.py
@@ -66,16 +66,34 @@ def unpickle(file):
 
 
 def init_dataset(path, dataset):  # path = kvs['args'].dataset_root
+        if 'CIFAR10' == dataset:
+        train_list = [
+            ['data_batch_1', 'c99cafc152244af753f735de768cd75f'],
+            ['data_batch_2', 'd4bba439e000b95fd0a9bffe97cbabec'],
+            ['data_batch_3', '54ebc095f3ab1f0389bbae665268c751'],
+            ['data_batch_4', '634d18415352ddfa80567beed471001a'],
+            ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'],
+        ]
+        #test_list = [
+        #   ['test_batch', '40351d587109b95175f43aff81a1287e'],
+        #]
+    elif 'CIFAR100' == dataset:
+        train_list = [
+            ['train', '16019d7e3df5f24257cddd939b257f8d'],
+        ]
+        #test_list = [
+        #   ['test', 'f0ef6b0ae62326f3e7ffdfab6717acfc'],
+        #]
+    else:
+        raise NotImplementedError
+
     path = os.path.join(path, dataset)
-    files = os.listdir(path)
-    files = sorted(files)
     ds = []
-    for file in files:
-        if file.startswith('data'):
-            print(colored('====> ', 'blue') + 'Processing file: ', os.path.join(path, file))
-            batch = unpickle(os.path.join(path, file))
-            tmp = batch[b'data']
-            ds.append(tmp)
+    for entry in train_list:
+        print(colored('====> ', 'blue') + 'Processing file: ', os.path.join(path, entry[0]))
+        batch = unpickle(os.path.join(path, entry[0]))
+        tmp = batch[b'data']
+        ds.append(tmp)
 
     ds = np.concatenate(ds)
     ds_len = len(ds)
